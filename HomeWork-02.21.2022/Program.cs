2bebb027ec25a3ff49bfa307d5079c9370bf9cd7
sadiqli1 <isaas@code.edu.az>
2022-02-21 01:03:09 +0400

    created folder

---
+++ HomeWork-02.21.2022/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeWork_02._21._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //User user = new User("sadig0v_1","Isa2002");
+            //Console.WriteLine(user.GetInfo());
+
+            //Admin admin = new Admin("sadig0v_1", "Isa2002",true,"asdf");
+            //Console.WriteLine(admin.getInfo());
+
+            //User user = new User("sadigov","asdfg123");
+            ////Console.WriteLine(user.GetInfo());
+            //user.setUsername("sa");
+            ////user.setPassword("asdfggh");
+            //Console.WriteLine(user.GetInfo());
+
+            Admin admin = new Admin("sa","123456",false,"add");
+            Console.WriteLine(admin.GetInfo());
+        }
+    }
+}
---
+++ HomeWork-02.21.2022/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_02._21._2022
+{
+    internal class User
+    {
+        private string Username;
+        private string Password;
+
+        public User(string username,string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public void setUsername(string username)
+        {
+            if (username.Length>6)
+            {
+                Username = username;
+            }
+            else
+            {
+                Console.WriteLine("Zehmet olmasa username-ni duzgun daxil edin");
+                Username = Console.ReadLine();
+            }
+        }
+        //public void setPassword(string password)
+        //{
+        //    foreach (char symbol in password)
+        //    {
+        //        if (symbol ==(1-9) )
+        //        {
+        //            Password = password;
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("Zehmet olmasa paswword-u duzgun daxil edin");
+        //            Password = Console.ReadLine();
+        //        }
+        //    }
+
+        //}
+
+        public string getinfo()
+        {
+            return $"UserName:{Username}\nPasword:{Password}";
+        }
+    }
+}
